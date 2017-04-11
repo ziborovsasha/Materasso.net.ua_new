643262af16398687987061f247863ca2d7ab4c59
ziborovsasha <ziborov_sasha@ukr.net>
2017-04-11 16:07:35 +0300

    11.04.17

---
+++ js/jquery.main.js
@@ -0,0 +1,1 @@
+$(function(){	$('.slider-sale').slick({		// slidesToScroll: 1,		// // dots: true,		// autoplay: true,		// autoplaySpeed: 2500		// prevArrow: '<button type="button" class="slick-prev">Previous</button>',		// nextArrow: '<button type="button" class="slick-next">Next</button>'	});		});
