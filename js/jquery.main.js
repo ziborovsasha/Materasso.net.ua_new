783addb2b398c7b0c4137675ca4d740d22d66197
ziborovsasha <ziborov_sasha@ukr.net>
2017-04-15 15:38:45 +0300

    !!!!

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-$(function(){	$('.slider-sale').slick({		slidesToScroll: 1,		// dots: true,		autoplay: true,		autoplaySpeed: 2500,		prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',		nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'	});		});
-=======
-$(function(){	$('.slider-sale').slick({		// slidesToScroll: 1,		// // dots: true,		// autoplay: true,		// autoplaySpeed: 2500		prevArrow: '<span class="slick-prev"></span>',		nextArrow: '<span class="slick-next"></span>'	});});
->>>>>>> origin/master
+$(function(){	$('.slider-sale').slick({		slidesToScroll: 1,		// dots: true,		autoplay: true,		autoplaySpeed: 2500,		prevArrow: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',		nextArrow: '<i class="fa fa-chevron-right" aria-hidden="true"></i>'	});		});
